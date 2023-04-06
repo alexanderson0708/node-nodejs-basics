0915458423abf20f354c2cf6b1b0194acd5bf547
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 11:36:24 +0500

    feat: add solution to streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { EOL } from 'node:os';
+import { pipeline, Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformData = new Transform({
+        transform(chunk, encoding, callback){
+            callback(null, 
+                chunk.toString()
+                .replace(EOL,'')
+                .split('').reverse().join('')+EOL)
+        },
+    })
+    pipeline(
+        process.stdin,
+        transformData,
+        process.stdout,
+        (err)=>{
+            console.error(err);
+        }
+    )
 };
 
 await transform();
