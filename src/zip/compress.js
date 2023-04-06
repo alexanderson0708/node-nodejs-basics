952ee77496b180288dfdbf27f6ef24b7fc2503e0
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 11:37:51 +0500

    feat: add solution to zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import {fileURLToPath} from 'node:url'
+import { dirname } from 'node:path';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+import {createReadStream, createWriteStream} from 'node:fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const source = '/files/fileToCompress.txt'
+const newArchive = '/files/archive.gz'
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(__dirname+source)
+    const writeStream = createWriteStream(__dirname+newArchive)
+
+    pipeline(
+        readStream,
+        createGzip(),
+        writeStream,
+        (err)=>{
+            console.error(err);
+        }
+    ) 
 };
 
 await compress();
