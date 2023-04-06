b8b549d63eacab31679e48ae67e84b8620cb543c
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 09:38:50 +0500

    feat: add solution to read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import { URL } from 'node:url';
+import { ERROR_MSG, FILE_PATH } from './consts.js';
+
+const fileName = 'fileToRead.txt'
+
 const read = async () => {
-    // Write your code here 
+    const getAbsPath = (path) => new URL(path,import.meta.url)
+    const url = getAbsPath(`${FILE_PATH}/${fileName}`)
+    try {
+        let content = await readFile(url, 'utf-8')
+        console.log(content);
+    }catch (err){
+        console.log(err);
+        throw Error (ERROR_MSG)  
+    }
 };
 
 await read();
