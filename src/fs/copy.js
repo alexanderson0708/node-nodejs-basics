2572ac610de8bc5d22fd3066683f22721c7b0e4c
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 09:28:08 +0500

    feat: add solution to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { stat } from 'fs';
+import {cp} from 'fs/promises'
+import { URL } from 'node:url';
+import { ERROR_MSG, FILE_PATH } from './consts.js';
+
+const fileName = 'files_copy'
+
 const copy = async () => {
-    
+    const getAbsPath = (path) => new URL(path,import.meta.url)
+    const src = getAbsPath(`${FILE_PATH}`)
+    const dest = getAbsPath(`${fileName}`)
+    try {
+        stat(src, (err) => {
+            console.log(ERROR_MSG);
+        })
+        await cp(src, dest, {errorOnExist:true, recursive:true, force:false})
+    }catch (err){
+        console.log(err);
+        throw Error (ERROR_MSG)  
+    }
 };
 
 await copy();
