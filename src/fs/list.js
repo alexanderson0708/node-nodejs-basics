073f6fbfdcaf0f6c1372c5b1b588a81d2e4596e3
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 09:37:11 +0500

    feat: add solution to list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir} from 'node:fs/promises';
+import { URL } from 'node:url';
+import { ERROR_MSG, FILE_PATH } from './consts.js';
+
 const list = async () => {
-    // Write your code here 
+    const getAbsPath = (path) => new URL(path,import.meta.url)
+    const url = getAbsPath(`${FILE_PATH}`)
+    try {
+        let files = await readdir(url)
+        files.forEach((file)=>{
+            console.log(file);
+        })
+    }catch (err){
+        console.log(err);
+        throw Error (ERROR_MSG)  
+    }
 };
 
 await list();
