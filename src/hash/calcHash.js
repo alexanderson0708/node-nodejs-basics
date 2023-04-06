6f577a286d7e5b47bfe2fa26e631c1f42b5a3f4a
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 10:05:56 +0500

    feat: add solution to hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import {createHash} from 'node:crypto'
+import { readFile } from 'node:fs/promises';
+
+const fileName = 'fileToCalculateHashFor.txt'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const getAbsPath = (path) => new URL(path,import.meta.url)
+    const url = getAbsPath(`files/${fileName}`)
+    const content = await readFile(url)
+    const hash = createHash('sha3-256').update(content).digest('hex')
+    console.log('Hash:',hash)
 };
 
 await calculateHash();
