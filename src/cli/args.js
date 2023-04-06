408896157e88ff744a25d8bb71131888516afaa1
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 09:41:09 +0500

    feat: add solution to cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    console.log(args);
+    const newArr = args.reduce((acc, arg, index, arr)=>{
+        if (arg.startsWith('--') && arr[index+1]){
+            acc.push(`${arg.slice(2)} is ${arr[index+1]}`)
+        }
+        return acc
+    },[])
+    console.log(newArr.join(', '));
 };
 
 parseArgs();
