b4c0d3f2ac2b9e1235dbbae6ec1e65e674bebd0a
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 09:33:38 +0500

    feat: add solution to rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import { rename as renameFile } from 'node:fs/promises';
+import { URL } from 'node:url';
+import { ERROR_MSG, FILE_PATH } from './consts.js';
+
+const fileName = 'wrongFilename.txt'
+const newFilename = 'properFilename.md'
+
 const rename = async () => {
-    // Write your code here 
+    const getAbsPath = (path) => new URL(path,import.meta.url)
+    const url = getAbsPath(`${FILE_PATH}/${fileName}`)
+    const newUrl = getAbsPath(`${FILE_PATH}/${newFilename}`)
+    try {
+        await renameFile(url, newUrl)
+    }catch (err){
+        console.log(err);
+        throw Error (ERROR_MSG)  
+    } 
 };
 
 await rename();
