892b126b7571521d2fe74f9b5d5c39e717353ea3
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-04-06 09:24:33 +0500

    feat: add solution to create.js

---
+++ src/fs/consts.js
@@ -0,0 +1,2 @@
+export const ERROR_MSG = 'FS operation failed'
+export const FILE_PATH = 'files'
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import {writeFile} from 'fs/promises'
+import { URL } from 'node:url';
+import { ERROR_MSG, FILE_PATH } from './consts.js';
+
+const fileName = 'fresh.txt'
+const content = 'I am fresh and young'
+
 const create = async () => {
-    // Write your code here 
+    const getAbsPath = (path) => new URL(path,import.meta.url)
+    const url = getAbsPath(`${FILE_PATH}/${fileName}`)
+    try {
+        await writeFile(url, content, {flag: 'wx'})
+    }catch (err){
+        console.log(err);
+        throw Error (ERROR_MSG)  
+    } 
 };
 
 await create();
